645dae7ab7508df1490026df451cd32aa9218f02
Derya Inanici <dinanici@blurb.com>
2016-05-24 21:29:21 -0700

    class 5/24 fashion blog advanced + compare js

--- code-alongs/compare/js/compare.js
+++ code-alongs/compare/js/compare.js
@@ -1,17 +1,43 @@
 // Structure
-
+//----------------------------------
 var firstNumber = document.querySelector (".first-number");
 var secondNumber = document.querySelector (".second-number");
 var sign = document.querySelector (".sign");
 var compare = document.querySelector (".compare");
 
-// Events
 
-compare.addEventListener('click', compareNumbers);
+// Events
+//--------------------------
+
+// Listen for the compare button to be clicked
+compare.addEventListener("click", compareNumbers);
+
+
 
 // Event Handlers
+//--------------------------------------
+// Compare the first and second number
 
 function compareNumbers () {
-	sign.textContent = "="
-}
 
+	var firstValue = firstNumber.value;
+	firstValue = parseInt(firstValue);
+
+	var secondValue = secondNumber.value;
+	secondValue = parseInt(secondValue);
+
+	// update the page - change the sign
+
+	// JS Conditional (if, else if, else)
+	if (firstValue > secondValue) {
+		sign.textContent = ">"
+	} 
+	// JS Comparison (greater than, less than)		
+	else if (firstValue < secondValue) {
+		sign.textContent = "<";
+	}
+
+	else if (firstValue == secondValue) {
+		sign.textContent = "=";
+	}
+}
