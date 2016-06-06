a91f892ebd19a7e7164fd7efeab0199cada6fdfa
Derya Inanici <dinanici@blurb.com>
2016-06-05 22:11:54 -0700

    cash register - fixed rounding

--- homework/cash-register/js/cash-register.js
+++ homework/cash-register/js/cash-register.js
@@ -40,26 +40,37 @@ function addItem(e) {
 
 // Update page functions
 // ------------------------------------------------
-
+// price is an argument which stores the value, which was passed in,
+// and assigns it a local name, which is price
+// in this case the value is going to be input.value from line 27, because
+// the createItem function was called on line 27 and the value of 
+// input.value was passed into it
 function createItem(price) {
 	var li = document.createElement('li'); 
 	li.textContent = "$" + price;
 	entries.appendChild(li);
 }
 
-//----------------------------------------------
-// calculate total 
-
-function updateTotal() {
-	var newTotal = 0
-
-	for (var index = items.length - 1; index >= 0; index--) {
-		newTotal = newTotal + parseInt(items[index])
-	}
-
-	total.textContent = "$" + newTotal;
-}
 
 // Utility functions
 // ------------------------------------------------
 
+function updateTotal() {
+	var newTotal = 0
+
+	// for = run the loop
+	// var index = 0; = set index variable to 0
+	// index <= items.length - 1; = run loop for as long as it's smaller or equal to the total -1 (beacuse the position/index starts at 0, but the count/items starts at 1)
+	// index = index + 1 = add 1 to the index every time the loop runs, which the loop when to stop running
+	for (var index = 0; index <= items.length - 1; index = index + 1) {
+
+	// every time the loop runs within the function add the next item to the newTotal, which started at 0.
+	// use parseFloat instead of parseInt because it's decimals. Integer = ganze Zahl. 
+		newTotal = newTotal + parseFloat(items[index])
+		console.log("newTotal: " + newTotal)
+		console.log("index: " + index)
+	}
+
+// when the loop is done running update the page with a string representation of the dollar amount 
+	total.textContent = "$" + newTotal;
+}
